7a77a18f8d67710cb5a438be3c2f368f7bad3e08
Chris Forbes <chris.forbes@catalyst.net.nz>
2012-08-09 08:42:33 +1200

    simplify posting of events back to the server

--- static/js/app.js
+++ static/js/app.js
@@ -1,24 +1,27 @@
 $(function(){
 
+    var post_event = function(uri, data) {
+        $.ajax(uri, {
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(data),
+            success: function(data) { poll_event(true); }
+        });
+    };
+
     /* set up dragging of lists */
     $('#main').sortable({
         tolerance:'pointer',
         placeholder:'list-placeholder',
         forcePlaceholderSize: true,
-        });
+    });
 
     $('#main').bind( 'sortupdate', function(ev,ui) {
-        var update = {
+        post_event( '/list/move', {
             'list': ui.item.attr('id'),
             'after': ui.item.prev().attr('id') || null,
-        };
-        $.ajax( '/list/move', {
-            type: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify(update),
-            success: function(data) { poll_event(true); }
-            });
         });
+    });
 
     /* set up dragging of cards around */
     $('div.list-body').sortable({
@@ -26,66 +29,46 @@ $(function(){
         placeholder:'card-placeholder',
         forcePlaceholderSize: true,
         connectWith: 'div.list-body',
-        }).disableSelection();
+    }).disableSelection();
 
     $('div.list-body').bind( 'sortupdate', function(ev,ui) {
+        ev.stopPropagation();
+
         if (ui.item.closest('div.list').attr('id') != $(this).closest('div.list').attr('id')) {
-            ev.stopPropagation();
             return;
         }
 
-        var update = {
+        post_event( '/card/move', {
             'list': ui.item.closest('div.list').attr('id'),
             'card': ui.item.attr('id'),
             'after': ui.item.prev().attr('id') || null,
-        };
-
-        $.ajax( '/card/move', {
-            type: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify(update),
-            success: function(data) { poll_event(true); }
-            });
-
-        ev.stopPropagation();   /* if we let this continue to bubble, #main's handler
-                                    gets involved too; we don't want that. */
         });
+    });
 
     /* set up add machinery */
-    $(document).on('click', 'div.list-footer',
-        function(ev,ui) {
-            $(this).prev('div.add-machinery').show()
-                .find('textarea').focus();
+    $(document).on('click', 'div.list-footer', function(ev,ui) {
+        $(this).prev('div.add-machinery').show()
+            .find('textarea').focus();
+    });
+
+    $(document).on('submit', 'div.add-machinery', function() { return false; });
+
+    $(document).on('click', 'div.add-machinery :reset', function(ev,ui) {
+        $(this).closest('div.add-machinery').hide();
+    });
+
+    $(document).on('click', 'div.add-machinery :submit', function(ev,ui) {
+        var form = $(this).closest('div.add-machinery');
+        var list = $(this).closest('div.list');
+
+        post_event('/card/new', {
+            'list': list.attr('id'),
+            'label': form.find('textarea').val()
         });
 
-    $(document).on('submit', 'div.add-machinery',
-        function() { return false; });
-
-    $(document).on('click', 'div.add-machinery :reset',
-        function(ev,ui) {
-            $(this).closest('div.add-machinery').hide();
-        });
-
-    $(document).on('click', 'div.add-machinery :submit',
-        function(ev,ui) {
-            var form = $(this).closest('div.add-machinery');
-            var list = $(this).closest('div.list');
-
-            var update = {
-                'list': list.attr('id'),
-                'label': form.find('textarea').val()
-            };
-
-            form.find('textarea').val('');
-            form.hide();
-
-            $.ajax( '/card/new', {
-                type: 'POST',
-                contentType: 'application/json',
-                data: JSON.stringify(update),
-                success: function(data) { poll_event(true); },
-                });
-        });
+        form.find('textarea').val('');
+        form.hide();
+    });
 
     var handle_event = function(ev) {
         if (last_event_id >= ev.id) return;
